2b64a2e48c56861b46fe00501c59992155b25e99
tuka0613s <tuka0613s@gmail.com>
2026-01-11 19:39:44 +0900

    Add Zephyr kernel and print loop in main.c

--- myprj/src/main.c
+++ myprj/src/main.c
@@ -1,1 +1,13 @@
+#include <zephyr/kernel.h>
+#include <stdio.h>
 
+int main(void)
+{
+	printf("Hello World! from Zephyr CI/CD\n");
+
+	while (1) {
+		k_msleep(1000);
+		printf("Running...\n");
+	}
+	return 0;
+}
